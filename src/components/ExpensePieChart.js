1c5618349633c0f842b654b8376e32bb4e8c4b0f
Akanksha <akankshakumari2715@gmail.com>
2026-06-11 19:33:15 +0530

    Fix Vercel deployment build

--- src/components/ExpensePieChart.js
+++ src/components/ExpensePieChart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from 'recharts';
 
 function ExpensePieChart({ summary, loading }) {
   if (loading) {
